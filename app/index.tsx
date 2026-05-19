671385df3dbd3460c9f3673445fc68f8204db704
tsubasa <c0a2417498@edu.teu.ac.jp>
2026-05-19 22:12:19 +0900

    基本機能及び概形

---
+++ app/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Todoリストアプリ開発スタート！</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
